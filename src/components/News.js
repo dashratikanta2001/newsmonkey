35a8f8c9b34854607bf72aef9d2bde6cd9f8f6b6
Ratikanata <ratikantadash2001@gmail.com>
2022-04-09 03:04:05 +0530

    Design update

--- src/components/News.js
+++ src/components/News.js
@@ -4,68 +4,58 @@ import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 // let originalapi = "2256ad5e5f244b52a3630a37c3b7b837"
 export class News extends Component {
-    
+
     static defaultProps = {
-        country:'in',
+        country: 'in',
         pageSize: 8,
-        category : 'general'
+        category: 'general'
     }
 
     static propTypes = {
         country: PropTypes.string,
-        pageSize : PropTypes.number,
-        category : PropTypes.string
+        pageSize: PropTypes.number,
+        category: PropTypes.string
     }
-    constructor() {
-        super();
+    capitalizeFirstLetter = (string) =>{
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    constructor(props) {
+        super(props);
         this.state = {
             articles: [],
             loading: false,
             page: 1
         }
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
     }
-    async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6491759904354a60a913e5929818df26&page=1&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true});
+    async updateNews() {
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6491759904354a60a913e5929818df26&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
         let data = await fetch(url);
         let parsedata = await data.json()
         this.setState({
-             articles: parsedata.articles, 
-             totalResults: parsedata.totalResults,
-             loading: false
-            })
-    }
-    handlePrevClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6491759904354a60a913e5929818df26&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true});
-        let data = await fetch(url);
-        let parsedata = await data.json()
-
-        this.setState({
-            page: this.state.page - 1,
             articles: parsedata.articles,
+            totalResults: parsedata.totalResults,
             loading: false
         })
     }
+    async componentDidMount() {
+        this.updateNews()
+    }
+    handlePrevClick = async () => {
+        this.setState({ page: this.state.page - 1 })
+        this.updateNews()
+    }
     handleNextClick = async () => {
-        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6491759904354a60a913e5929818df26&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            this.setState({loading:true});
-            let data = await fetch(url);
-            let parsedata = await data.json()
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedata.articles,
-                loading: false
-            })
-        }
+        this.setState({ page: this.state.page + 1 })
+        this.updateNews()
     }
     render() {
         return (
             <div className="container my-3">
-                <h1 className='text-center my-3'>NewsMonkey - Top Headlines</h1>
+                <h1 className='text-center my-3'>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {/* {this.state.loading && <Spinner/>} */}
-                {this.state.loading && <Spinner/>}
+                {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div key={element.url} className="col-md-3">
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,6 @@ export class NewsItem extends Component {
                     <div className="card-body">
                         <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:"80%", zIndex:"1"}}>
                             {source}
-                            <span className="visually-hidden">unread messages</span>
                         </span>
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
